f38cbb91678ad8429eb754f95c1eff3a38d096f6
Sankarapu Samhith <58435062+sankarapusamhith@users.noreply.github.com>
2020-10-10 22:17:52 +0530

    Add files via upload

---
+++ day 2/employee/src/com/company/employee.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class employee {
+    int age=23;
+    String name ="saurabh";
+    String city="chennai";
+    public void display()
+    {
+        System.out.println("The name is "+name);
+        System.out.println("The age is "+age);
+        System.out.println("The city is "+city);
+
+    }
+}
